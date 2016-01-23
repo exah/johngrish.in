0911aec42773606a21d46fd27c44b9b79254d7a4
John Grishin <r@exah.ru>
2016-01-23 18:08:50 +0300

    Build tools

---
+++ src/index.js
@@ -0,0 +1,10 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-51202783-1', 'johngrish.in');
+ga('send', 'pageview');
+
+if (window.console)
+    console.log("%c                        XXX              \n                     XXXXXXX             \n                  XXXXXXXXXXX            \n               XXXXXXXXXXXXXXX           \n            XXXXXXXXXXXXXXXXXXX          \n         XXXXXXXXXXXXXXXXXXXXXXX         \n      XXXXXXXXXXXXXXXXXXXXXXXXXXX        \n   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX       \nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX      \n XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX     \n  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX    \n   XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   \n    XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  \n     XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX \n      XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n       XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX   \n        XXXXXXXXXXXXXXXXXXXXXXXXXXX      \n         XXXXXXXXXXXXXXXXXXXXXXX         \n          XXXXXXXXXXXXXXXXXXX            \n           XXXXXXXXXXXXXXX               \n            XXXXXXXXXXX                  \n             XXXXXXX                     \n              XXX                        ", "color: black; font-weight: bold;");
